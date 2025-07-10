4c13658dd8cd6f43b5b4b7de06868cd4c13acb3c
nyradhr <alessio.piccione@hotmail.it>
2025-07-10 17:58:12 +0200

    wip caching

---
+++ internal/pokecache.go
@@ -0,0 +1,58 @@
+package internal
+
+import (
+	"sync"
+	"time"
+)
+
+type Cache struct {
+	entries  map[string]cacheEntry
+	mu 		 sync.Mutex
+	interval time.Duration
+}
+
+type cacheEntry struct {
+	createdAt time.Time
+	val []byte
+}
+
+func NewCache(interval time.Duration) *Cache {
+	c:= Cache{
+		entries: make(map[string]cacheEntry),
+		interval: interval
+	}
+	go c.reapLoop()
+	return &c
+
+}
+
+func (c *Cache) Add(key string, val []byte) {
+	c.mu.Lock()
+    defer c.mu.Unlock()
+	c.entries[key] = cacheEntry{createdAt: time.Now(), val: val}
+}
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mu.Lock()
+    defer c.mu.Unlock()
+	entry, ok := c.entries[key]
+	return entry.val, ok
+}
+
+func (c *Cache) reapLoop() {
+
+	ticker := time.NewTicker(c.interval)
+
+	go func() {
+        for t := range ticker.C {
+            for key, entry := range c.entries {
+				now := time.Now()
+				if now.Sub(entry.createdAt) > c.interval {
+					c.mu.Lock()
+					delete(c.entries, key);
+					c.mu.Unlock()
+				}
+			}
+        }
+    }()
+}
